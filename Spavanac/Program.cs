63f76fa2bec453a2525cd20393519270d15d274d
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-19 22:20:18 +0200

    Add project files.

---
+++ Spavanac/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Spavanac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myTime = EnterHoursMinutes();
+
+            var result = Make24MinutesEarly(myTime);
+
+            string str = $"{result[0]} {result[1]}";
+
+            Console.WriteLine(str);
+        }
+        private static int[] Make24MinutesEarly(int[] yourTime)
+        {
+            // int fullTime = 24 * 60;
+            var yourHours = yourTime[0];
+            var yourMinutes = yourTime[1];
+
+            var wholeMinutes = yourHours * 60 + yourMinutes;
+
+            // var earlyWholeMinutes = Math.Abs((wholeMinutes - 45) % fullTime);
+            var earlyWholeMinutes = (wholeMinutes - 45);
+
+            int nHours=0, nMinutes=0;
+
+            if (earlyWholeMinutes > 0)
+            {
+                nHours = earlyWholeMinutes / 60;
+                nMinutes = earlyWholeMinutes - nHours * 60;
+            }
+            else
+            {
+                nHours = 23;
+                nMinutes = 15 + wholeMinutes;
+            }
+
+            int[] result = new int[2] { nHours, nMinutes };
+            return result;
+        }
+
+        private static int[] EnterHoursMinutes()
+        {
+            string[] arr = new string[2] { "", "" };
+            int[] ans = new int[2] { 0, 0 };
+
+            try
+            {
+                arr = Console.ReadLine().Split(' ', 2);
+                for (int k = 0; k < arr.Length; k++)
+                {
+                    ans[k] = int.Parse(arr[k]);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return EnterHoursMinutes();
+            }
+            return ans;
+        }
+    }
+}
